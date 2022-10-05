cf001795e3a34e24aefea5f6c75adc2ebc44dcb4
EmilianoDuraku1 <emiliano.duraku@rws-solutions.com>
2022-10-05 17:17:23 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import * as fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import * as path from "path";
+import * as urlParser from "url";
+
+const seenUrls = {};
+const seenImg = {};
+
+const getUrl = (link, host, protocol) => {
+  if (link.includes("http")) {
+    return link;
+  } else if (link.startsWith("/")) {
+    return `${protocol}//${host}${link}`;
+  } else {
+    return `${protocol}//${host}/${link}`;
+  }
+};
+
+const crawl = async ({ url, ignore }) => {
+  if (seenUrls[url]) return;
+  seenUrls[url] = true;
+  console.log("crawling Links", { url });
+  const { host, protocol } = urlParser.parse(url);
+
+  const response = await fetch(url);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  const links = $("a")
+    .map((i, link) => link.attribs.href)
+    .get();
+  const imageUrls = $("img")
+    .map((i, link) => link.attribs.src)
+    .get();
+  seenImg[imageUrls] = true;
+  console.log("crawling Images", { imageUrls });
+
+  // This function is to save pictures in a folder in project
+  // But doesn't work
+  imageUrls.forEach((imageUrl) => {
+    fetch(getUrl(imageUrl, host, protocol)).then((response) => {
+      const filename = path.basename(imageUrl);
+      const dest = fs.createWriteStream(`images/${filename}`);
+      response.body.pipe(dest);
+    });
+  });
+
+  links
+    .filter((link) => link.includes(host) && !link.includes(ignore))
+    .forEach((link) => {
+      crawl({
+        url: getUrl(link, host, protocol),
+        ignore,
+      });
+    });
+};
+
+crawl({
+  url: "https://www.eucerin.nl/",
+  ignore: "/search",
+});
